01395a0b8c3061a395eb63479a8105e32cccc4a8
Yasmin Gabriele Xavier <ygxavier.yx@gmail.com>
2024-11-11 08:20:56 -0300

    Implementação teórica em aula concluída

--- Bootcamp/src/Main.java
+++ Bootcamp/src/Main.java
@@ -1,7 +1,9 @@
 import java.time.LocalDate;
 
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
 import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.Dev;
 
 public class Main {
     public static void main(String[] args) {
@@ -20,8 +22,35 @@ public class Main {
         mentoria1.setDescricao("descrição mentoria java");
         mentoria1.setData(LocalDate.now());
         
-        System.out.println(curso1);
-        System.out.println(curso2);
-        System.out.println(mentoria1);
+        // System.out.println(curso1);
+        // System.out.println(curso2);
+        // System.out.println(mentoria1);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria1);
+
+        Dev devYas = new Dev();
+        devYas.setNome("Yasmin");
+        devYas.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + devYas.getConteudosInscritos());
+        devYas.progredir();
+        System.out.println("Conteudos Concluidos" + devYas.getConteudosConcluidos());
+        System.out.println("XP: " + devYas.calcularTotalXp());
+
+
+        Dev joao = new Dev();
+        joao.setNome("João");
+        joao.inscreverBootcamp(bootcamp);
+        System.out.println("Conteudos Inscritos" + joao.getConteudosInscritos());
+        joao.progredir();
+        System.out.println("Conteudos Concluidos" + joao.getConteudosConcluidos());
+        System.out.println("XP: " + joao.calcularTotalXp());
+
+
+
     }
 }
--- Bootcamp/src/br/com/dio/desafio/dominio/Dev.java
+++ Bootcamp/src/br/com/dio/desafio/dominio/Dev.java
@@ -1,17 +1,35 @@
 package br.com.dio.desafio.dominio;
 import java.util.LinkedHashSet;
 import java.util.Set;
+import java.util.Optional;
+import java.util.Objects;
 
 public class Dev {
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
     private Set<Conteudo> conteudosConcluidos = new LinkedHashSet<>();
 
-    public void inscreverBootcamp(Bootcamp bootcamp) {}
+    public void inscreverBootcamp(Bootcamp bootcamp) {
+        this.conteudosInscritos.addAll(bootcamp.getConteudos());
+        bootcamp.getDevsInscritos().add(this);
+    }
 
-    public void progredir(){};
+    public void progredir(){
+        Optional<Conteudo> conteudo = this.conteudosInscritos.stream().findFirst();
+        if (conteudo.isPresent()) {
+            this.conteudosConcluidos.add(conteudo.get());
+            this.conteudosInscritos.remove(conteudo.get());  
+        } else {
+            System.err.println("Você não está matriculado em nenhum conteúdo!");
+        }
+    };
 
-    public void calcularTotalXp(){}
+    public double calcularTotalXp(){
+        return this.conteudosConcluidos
+        .stream()
+        .mapToDouble(Conteudo::calcularXp)
+        .sum();
+    }
 
     public String getNome() {
         return nome;
